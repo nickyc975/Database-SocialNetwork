565c18f7211cee9b81850ccc24ace545e3ba7ca2
nickyc975 <chenjinlong2016@gmail.com>
2019-04-12 21:40:28 +0800

    Added serialVersionUID.

--- src/main/java/exceptions/InvalidDataException.java
+++ src/main/java/exceptions/InvalidDataException.java
@@ -1,6 +1,8 @@
 package exceptions;
 
 public class InvalidDataException extends Exception {
+    private static final long serialVersionUID = 99878912987503L;
+
     public InvalidDataException(String invalidData, String detail) {
         super("Invalid data: \"" + invalidData + "\"\n" + detail);
     }
